4673d3bfcf626a8530ee2ab3d7c35fde105542f0
Ankita Arora <ankita@Ankitas-MacBook-Pro.local>
2016-05-29 01:45:53 +0530

    add list view & if else stmnt

--- app/src/main/java/com/example/ankita/fav5/DetailActivity.java
+++ app/src/main/java/com/example/ankita/fav5/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.ankita.fav5;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -13,15 +15,25 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         String hobbyName = getIntent().getStringExtra("view_name");
-        String hobbyDetails = MainActivity.hobbyDetailsMap.get(hobbyName);
+        String[] hobbyquotesArray;
+        if(MainActivity.hobbyDetailsMap.containsKey(hobbyName)) {
+            hobbyquotesArray = MainActivity.hobbyDetailsMap.get(hobbyName);
+        }
+        else{
+            hobbyquotesArray =new String[]{"Topic not found"};
+        }
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_listview, hobbyquotesArray);
 
-        displayDetails(hobbyDetails);
+
+        ListView listView = (ListView) findViewById(R.id.quotes_list);
+        listView.setAdapter(adapter);
+        displayTitle(hobbyName);
     }
 
 
-    private void displayDetails(String message) {
-        TextView detailTextView = (TextView) findViewById(R.id.detail_text_view);
-        detailTextView.setText(message);
+    private void displayTitle(String message) {
+        TextView titleTextView = (TextView) findViewById(R.id.title_textview);
+        titleTextView.setText(message);
     }
 
 }
